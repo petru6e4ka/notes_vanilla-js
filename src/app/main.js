5f7f65c9788a3f90d4ef697aa208a7049e98afa6
Anastasiia Vorobiova <petru6e4ka@gmail.com>
2023-08-03 00:59:48 +0300

    feature/first-data-load-notif

--- src/app/main.js
+++ src/app/main.js
@@ -2,10 +2,13 @@ import 'assets/scss/main.scss';
 import { service } from './db';
 import NoteList from './components/NotesList/NoteList.model';
 import CategoriesList from './components/CategoriesList/CategoriesList.model';
+import Notification from './components/Notification/Notification.model';
 
 class App {
 	constructor() {
+		this.notifications = new Notification();
 		this.serviceInit();
+		this.initialDataRequest();
 		this.notesListInit();
 		this.categoriesListInit();
 	}
@@ -14,20 +17,42 @@ class App {
 		this.service = service;
 	}
 
+	initialDataRequest() {
+		try {
+			this.notesData = this.service.notes.getAll();
+			this.categoriesData = this.service.categories.getAll();
+		} catch (error) {
+			this.notesData = [];
+			this.categoriesData = [];
+			this.notifications.render({
+				status: 'error',
+				text: 'The server is not responding, please, try later',
+			});
+		}
+	}
+
 	notesListInit() {
-		this.notesData = this.service.notes.getAll() || [];
 		this.notesRoot = document.querySelector('#notes');
 
-		if (this.notesData && this.notesRoot) {
+		if (this.notesRoot) {
 			this.noteList = new NoteList(this.notesData, this.notesRoot);
 		}
 	}
 
 	categoriesListInit() {
-		this.categoriesData = this.service.categories.getAll() || [];
 		this.categoriesRoot = document.querySelector('#categories');
+		this.categoriesData = this.bindNotesToCategories();
 
-		this.categoriesData = this.categoriesData.map((elem) => ({
+		if (this.categoriesRoot) {
+			this.categoriesList = new CategoriesList(
+				this.categoriesData,
+				this.categoriesRoot
+			);
+		}
+	}
+
+	bindNotesToCategories() {
+		return this.categoriesData.map((elem) => ({
 			...elem,
 			active: this.notesData.filter(
 				(note) => note.active && note.category === elem.title
@@ -36,13 +61,6 @@ class App {
 				(note) => !note.active && note.category === elem.title
 			).length,
 		}));
-
-		if (this.categoriesData && this.categoriesRoot) {
-			this.categoriesList = new CategoriesList(
-				this.categoriesData,
-				this.categoriesRoot
-			);
-		}
 	}
 }
 
